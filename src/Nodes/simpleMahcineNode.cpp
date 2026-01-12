a0c4d03f08caf6971c41be64837e36b89ac6474d
Jakub Kruszewski <89195@student.pb.edu.pl>
2026-01-12 15:32:49 +0100

    add logic for syncing nodes

--- src/Nodes/simpleMahcineNode.cpp
+++ src/Nodes/simpleMahcineNode.cpp
@@ -1,56 +1,20 @@
 #include "simpleMahcineNode.hpp"
 
-#include "ImNodeFlow.h"
+#include <ImNodeFlow.h>
+
 #include <format>
 #include <string>
 
-SimpleMachineNode::SimpleMachineNode() : fuel(0), time(0)
+
+SimpleMachineNode::SimpleMachineNode() : fuel(0), time(0), inPins(), outPins{}
 {
 
-    ins.resize(2);
-    outs.resize(2);
-    ins[0].name = std::string(inName1);
-    ins[1].name = std::string(inName2);
-
-    outs[0].name = std::string(outName1);
-    outs[1].name = std::string(outName2);
-
-    auto LabelMatchFilter = [](ImFlow::Pin *out, ImFlow::Pin *in) -> bool
-    {
-        auto *outNode = dynamic_cast<SimpleMachineNode *>(out->getParent());
-        auto *inNode = dynamic_cast<SimpleMachineNode *>(in->getParent());
-        if (!outNode || !inNode)
-            return false;
-
-        size_t outIdx = out->getUid();
-        size_t inIdx = in->getUid();
-
-        return outNode->outs[outIdx].name == inNode->ins[inIdx].name;
-    };
-
+    ins = {{0, "Input 1"}, {0, "Input 2"}};
+    outs = {{0, "Output 1"}, {0, "Output 2"}};
     setTitle("Machine");
     setStyle(ImFlow::NodeStyle::cyan());
 
-    for (size_t i = 0; i < ins.size(); i++)
-    {
-        this->addIN_uid<Ingredient>(i, " ", Ingredient{0, ""}, LabelMatchFilter)
-            ->renderer([this, i](ImFlow::Pin *p)
-                       {
-                ImGui::Text("%s", this->ins[i].name.c_str());
-                ImGui::SameLine();
-                p->drawSocket();
-                p->drawDecoration(); });
-    }
-
-    for (size_t i = 0; i < outs.size(); i++)
-    {
-        this->addOUT_uid<Ingredient>(i, " ")
-            ->renderer([this, i](ImFlow::Pin *p)
-                       {
-            ImGui::Text("%s", this->outs[i].name.c_str());
-            p->drawSocket();
-            p->drawDecoration(); });
-    }
+    syncPins();
 }
 SimpleMachineNode::SimpleMachineNode(size_t time, size_t fuel, std::vector<Ingredient> ins, std::vector<Ingredient> outs)
 {
@@ -58,7 +22,6 @@ SimpleMachineNode::SimpleMachineNode(size_t time, size_t fuel, std::vector<Ingre
 auto SimpleMachineNode::draw() -> void
 {
 
-    update();
     ImGui::Text("UID: 0x%lX", this->getUID());
     ImGui::Text("Internal Pin Count: %d", (int)this->getIns().size() + (int)this->getOuts().size());
     ImGui::PushItemWidth(100.f);
@@ -77,33 +40,23 @@ auto SimpleMachineNode::draw() -> void
         fuel = static_cast<size_t>(std::max(0, fuelInput));
     }
 
-    auto formatInputFields = [&](const char *category, std::vector<Ingredient> &list, const char *catPrefix)
+    if (formatInputIngridients("Inputs:", "in", ins, this->getIns(), [this](uintptr_t uid)
+                               { this->dropIN(uid); }))
     {
-        ImGui::Text("%s", category);
-        for (auto i = 0; i < list.size(); i++)
-        {
-            ImGui::Text("%d:", i);
-            ImGui::SameLine();
-            auto tmp = static_cast<int>(list[i].amount);
-            if (ImGui::InputInt(std::format("##{}Amt{}", catPrefix, i).c_str(), &tmp))
-            {
-                list[i].amount = tmp >= 0 ? static_cast<size_t>(tmp) : list[i].amount;
-            }
-            ImGui::SameLine();
-            char buffer[SimpleMachineNode::TEXT_INPUT_MAX_LENGTH]{};
-            snprintf(buffer, sizeof(buffer), "%s", list[i].name.c_str());
-            if (ImGui::InputText(std::format("##{}Name{}", catPrefix, i).c_str(), buffer, sizeof(buffer)))
-            {
-                list[i].name = std::string(buffer);
-            }
-        }
-    };
-    formatInputFields("Inputs:", ins, "in");
-    formatInputFields("Outputs", outs, "out");
+        return;
+    }
+    if (formatInputIngridients("Output:", "out", outs, this->getOuts(), [this](uintptr_t uid)
+                               { this->dropOUT(uid); }))
+    {
+        return;
+    }
+
+    update();
 }
 
 auto SimpleMachineNode::update() -> void
 {
+    
     for (size_t i = 0; i < ins.size(); i++)
     {
         auto p = inPin(i);
@@ -132,4 +85,145 @@ auto SimpleMachineNode::update() -> void
             }
         }
     }
+}
+
+auto SimpleMachineNode::syncPins() -> void
+{
+ 
+    using Connection = std::pair<std::string, ImFlow::Pin*>;
+
+    std::vector<Connection> savedInLinks;
+    std::vector<Connection> savedOutLinks;
+    for (auto &pin : this->getIns()) {
+        if(pin->isConnected()) {
+            if(auto link = pin->getLink().lock()) {
+                auto uid = pin->getUid();
+                if(uid < ins.size()) {
+                    savedInLinks.push_back({ins[uid].name, link->left()});
+                }
+            }
+        }
+    }
+
+    for(auto &pin : getOuts()) {
+        if(pin->isConnected()) {
+            if(auto link = pin->getLink().lock()) {
+                auto uid = pin->getUid();
+                if(uid < outs.size()) {
+                    ImFlow::Pin* otherSide = link->right(); 
+                    savedOutLinks.push_back({outs[uid].name, otherSide});
+                }
+            }
+        }
+    }
+    std::vector<uintptr_t> inUids, outUids;
+    for(auto& p: this->getIns()) inUids.push_back(p->getUid());
+    for(auto& p: this->getOuts()) outUids.push_back(p->getUid());
+    for(auto id : inUids) this->dropIN(id);
+    for(auto id : outUids) this->dropOUT(id);
+    inPins.clear();
+    outPins.clear();
+
+    auto LabelMatchFilter = [this](ImFlow::Pin *out, ImFlow::Pin *in) -> bool
+    {
+        auto *outNode = dynamic_cast<SimpleMachineNode *>(out->getParent());
+        auto *inNode = dynamic_cast<SimpleMachineNode *>(in->getParent());
+        if (!outNode || !inNode)
+            return false;
+
+        if (out->getUid() >= outNode->outs.size() || in->getUid() >= inNode->ins.size())
+            return false;
+
+        return outNode->outs[out->getUid()].name == inNode->ins[in->getUid()].name;
+    };
+
+    for (size_t i = 0; i < ins.size(); i++)
+    {
+        auto p = this->addIN_uid<Ingredient>(i, " ", Ingredient{0, ""}, LabelMatchFilter)->renderer([this, i](ImFlow::Pin *p)
+                                                                                                    {
+            if (i < ins.size()) {
+                ImGui::Text("%s", this->ins[i].name.c_str());
+                ImGui::SameLine();
+                p->drawSocket();
+                p->drawDecoration();
+            } });
+        inPins.push_back(p);
+    }
+
+    for (size_t i = 0; i < outs.size(); i++)
+    {
+        auto p = this->addOUT_uid<Ingredient>(i, " ")->renderer([this, i](ImFlow::Pin *p)
+                                                                {
+            if (i < outs.size()) {
+                ImGui::Text("%s", this->outs[i].name.c_str());
+                p->drawSocket();
+                p->drawDecoration();
+            } });
+        outPins.push_back(p);
+    }
+
+    
+    for(size_t i = 0; i < ins.size(); i++) {
+        for(auto& conn : savedInLinks) {
+            if(conn.first == ins[i].name && conn.second) {
+                inPins[i]->createLink(conn.second);
+            }
+        }
+    }
+
+    for(size_t i=0; i < outs.size(); i++) {
+        for(auto& conn : savedOutLinks) {
+            if(conn.first == outs[i].name && conn.second) {
+                outPins[i]->createLink(conn.second);
+            }
+        }
+    }
+    
+}
+
+auto SimpleMachineNode::formatInputIngridients(const char *category, const char *prefix,
+                                               std::vector<Ingredient> &list, 
+                                               const std::vector<std::shared_ptr<ImFlow::Pin>>&pins, std::function<void(uintptr_t)> dropFunc) -> bool
+{
+    ImGui::Text("%s", category);
+    ImGui::SameLine();
+    if (ImGui::Button(std::format("+##Add{}", prefix).c_str()))
+    {
+        list.push_back({0, "New"});
+        this->syncPins();
+        return true;
+    }
+
+    for (auto i = 0; i < list.size(); i++)
+    {
+        ImGui::PushID(i);
+        ImGui::Text("%d:", i);
+        ImGui::SameLine();
+        auto tmp = static_cast<int>(list[i].amount);
+        if (ImGui::InputInt(std::format("##{}Amt{}", prefix, i).c_str(), &tmp))
+        {
+            list[i].amount = tmp >= 0 ? static_cast<size_t>(tmp) : list[i].amount;
+        }
+        ImGui::SameLine();
+        char buffer[SimpleMachineNode::TEXT_INPUT_MAX_LENGTH]{};
+        snprintf(buffer, sizeof(buffer), "%s", list[i].name.c_str());
+        if (ImGui::InputText(std::format("##{}Name{}", prefix, i).c_str(), buffer, sizeof(buffer)))
+        {
+            list[i].name = std::string(buffer);
+        }
+        ImGui::SameLine();
+        ImGui::PushStyleColor(ImGuiCol_Button, ImVec4(0.6f, 0.1f, 0.1f, 1.0f));
+        if (ImGui::Button(std::format("X##{}_Del_{}", prefix, i).c_str()))
+        {
+            dropFunc(pins[i]->getUid());
+            list.erase(list.begin() + i);
+            this->syncPins();
+            ImGui::PopStyleColor();
+            ImGui::PopID();
+            return true;
+        }
+        ImGui::PopStyleColor();
+        ImGui::PopID();
+    }
+    return false;
 }
--- src/Nodes/simpleMahcineNode.hpp
+++ src/Nodes/simpleMahcineNode.hpp
@@ -1,32 +1,32 @@
 #pragma once
 
 #include <ImNodeFlow.h>
+#include <memory>
 #include "Util/ingredient.hpp"
 
-class SimpleMachineNode : public ImFlow::BaseNode {
+class SimpleMachineNode : public ImFlow::BaseNode
+{
 private:
     size_t time;
     size_t fuel;
     std::vector<Ingredient> ins;
     std::vector<Ingredient> outs;
-
-    char inName1[64] = "Input 1";
-    char inName2[64] = "Input 2";
-    char outName1[64] = "Output 1";
-    char outName2[64] = "Output 2";
-    int inAmount1 = 0;
-    int inAmount2 = 0;
-    int outAmount1 = 0;
-    int outAmount2 = 0;
+    std::vector<ImFlow::Pin *> inPins;
+    std::vector<ImFlow::Pin *> outPins;
     int timeInput = 0;
     int fuelInput = 0;
 
 public:
     static const size_t TEXT_INPUT_MAX_LENGTH = 128;
 
+private:
+    auto formatInputIngridients(const char *category, const char *prefix, std::vector<Ingredient> &list,
+                                const std::vector<std::shared_ptr<ImFlow::Pin>> &pins, std::function<void(uintptr_t)> dropFunc) -> bool;
+
 public:
     SimpleMachineNode();
     SimpleMachineNode(size_t time, size_t fuel, std::vector<Ingredient> ins, std::vector<Ingredient> outs);
     auto draw() -> void override;
     auto update() -> void;
-};  
+    auto syncPins() -> void;
+};
